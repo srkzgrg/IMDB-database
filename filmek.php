a3cd3b446374feaaea1829f13b90856be983722c
srkzgrg <srkzgrg@outlook.com>
2022-09-08 19:45:22 +0200

    menu v1

---
+++ filmek.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="style.css">
+     <title>IMDB - Filmek tárháza</title>
+</head>
+
+<body>
+     <!------MENU------>
+     <ul>
+          <li><a href="filmek.php" class="active">Filmek</a></li>
+          <li><a href="szineszek.php">Színészek</a></li>
+          <li><a href="rendezok.php">Rendezők</a></li>
+          <li><a href="mufajok.php">Műfajok</a></li>
+     </ul>
+     <!------MENU------>
+</body>
+
+</html>
