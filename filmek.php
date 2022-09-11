90941e54fea83e0ca159b187e63f9d1ff8bca619
srkzgrg <srkzgrg@outlook.com>
2022-09-11 18:53:39 +0200

    filmek cards

--- filmek.php
+++ filmek.php
@@ -2,20 +2,20 @@
 session_start();
 $servername = "localhost";
 $username = "root";
-$password = "root";  
+$password = "root";
 $sql = "SELECT * FROM `filmek`";
 try {
      $db = new PDO("mysql:host=$servername;dbname=teszt", $username, $password);
      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      //echo "Connected successfully";
-   } catch(PDOException $e) {
+} catch (PDOException $e) {
      //echo "Connection failed: " . $e->getMessage();
-   }
+}
 
-   $query = $db->query($sql);
-   $resoult = $query->fetchAll( PDO::FETCH_ASSOC);
+$query = $db->query($sql);
+$resoult = $query->fetchAll(PDO::FETCH_ASSOC);
 
-   
+
 ?>
 
 
@@ -42,25 +42,26 @@ try {
      </nav>
      <!------MENU------>
      <?php
+     foreach ($resoult as $i){
      echo "<div class='courses-container'>
           <div class='course'>
                <div class='course-preview'>
-                    <h6>".$resoult[0]["mufaj"]."</h6>
-                    <h2>".$resoult[0]["cim"]."</h2>
-                    <p>View all chapters <i class='fas fa-chevron-right'></i></p>
+                    <h6>" . $i["mufaj"] . "</h6>
+                    <h2>" . $i["cim"] . "</h2>
+                    <p>2022 <i class='fass fa-chevron-right'></i></p>
                </div>
                <div class='course-info'>
-                    
-                    <h6>4 évad</h6>
-                    <h2>Decades after their 1984 All Valley Karate Tournament bout, a middle-aged Daniel LaRusso and Johnny Lawrence.....</h2>
+                    <h6>Leírás</h6>
+                    <h2>" . $i["leiras"] . "</h2>
                     <button class='btn'>Tovább</button>
+               </div>
+               <div class='review'>
+                    <h6>Értékelés</h6>
+                    <h2>5/10</h2>
                </div>
           </div>
      </div>";
-
+     }
      ?>
-
-     
 </body>
-
 </html>
